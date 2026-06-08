0d6ac98f1fa66dd001172492a11605fb7a410d47
Jose Limbert <85981548+jlimbert1999@users.noreply.github.com>
2026-06-07 22:33:45 -0400

    correccion ruta baseUrl desarrollo

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  baseUrl: 'http://localhost:7000',
-  identityHubUrl: 'http://localhost:4200',
+  baseUrl: 'http://localhost:8200',
+  identityHubUrl: 'http://localhost:8000',
 };
